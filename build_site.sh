7aa2abb90ffb4b12ecad041c14f60e89b4d6b9c1
William Born <william.born.git@gmail.com>
2024-08-20 17:18:22 -0400

    mdbook build

--- build_site.sh
+++ build_site.sh
@@ -1,2 +1,3 @@
-mdbook build book -d ../out/book
+curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.7/mdbook-v0.4.7-x86_64-unknown-linux-gnu.tar.gz | tar xvz
+./mdbook build book -d ../out/book
 cp -a site/* out/
