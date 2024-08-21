435eff92640680f2c559636bedc7344edd2840ff
William Born <william.born.git@gmail.com>
2024-08-21 08:56:36 -0400

    version 0.0.8

--- src/uncross/cli/uncross/new.py
+++ src/uncross/cli/uncross/new.py
@@ -12,9 +12,11 @@ from uncross.project.create import create_project
 LOGGER = make_logger(__name__)
 
 
-def new_command(name: str, source_dir: str, build_project: bool, git: bool) -> None:
+def new_command(name: str | None, source_dir: str, build_project: bool, git: bool) -> None:
     """new project"""
     LOGGER.debug("Creating new project %s at %s ...", name, source_dir)
+    if name is None:
+        name = source_dir
     create_project(name, source_dir, build_project, git)
 
 
